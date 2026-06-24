c12bdcd8c1f19df9b95fc425bf5e25f7bbb28dce
XTERMEGAMER <megagamer1236789@gmail.com>
2026-06-25 01:48:02 +0530

    Switch project to Yarn mappings

--- src/main/java/com/xterme/RedNetherite.java
+++ src/main/java/com/xterme/RedNetherite.java
@@ -2,29 +2,25 @@ package com.xterme;
 
 import com.xterme.item.RedNetheriteItems;
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class RedNetherite implements ModInitializer {
 
     public static final String MOD_ID = "xterme";
-
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     @Override
     public void onInitialize() {
-
         LOGGER.info("==================================");
-        LOGGER.info(" Red Netherite Mod Loaded!");
-        LOGGER.info(" Welcome XTERME!");
+        LOGGER.info("Red Netherite Mod Loaded!");
+        LOGGER.info("Welcome XTERME!");
         LOGGER.info("==================================");
 
         RedNetheriteItems.registerModItems();
     }
 
-   // FIXED ID SYSTEM
-public static ResourceLocation id(String path) {
-    return ResourceLocation.fromNamespaceAndPath(MOD_ID, path);
-}
+    public static Identifier id(String path) {
+    return Identifier.of(MOD_ID, path);
 }
